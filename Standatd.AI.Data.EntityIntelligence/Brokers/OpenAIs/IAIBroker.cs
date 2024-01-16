3b33754dd27916a6d013384d8bf86069ab509c5b
Zafar <icom@MACKBOOK>
2024-01-16 15:11:58 +0300

    BROKERS: Prompt AI Completion

---
+++ Standatd.AI.Data.EntityIntelligence/Brokers/OpenAIs/IAIBroker.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Standard.AI.OpenAI.Models.Services.Foundations.Completions;
+
+namespace Standatd.AI.Data.EntityIntelligence.Brokers.OpenAIs
+{
+    public interface IAIBroker
+    {
+        ValueTask<Completion> PromptCompletionAsync(Completion completion);
+    }
+}
---
+++ Standatd.AI.Data.EntityIntelligence/Models/Foundations/AIs/AIConfigurations.cs
@@ -0,0 +1,11 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+namespace Standatd.AI.Data.EntityIntelligence.Models.Foundations.AIs
+{
+    public class AIConfigurations
+    {
+        public string AIApiKey{ get; set; }
+    }
+}
