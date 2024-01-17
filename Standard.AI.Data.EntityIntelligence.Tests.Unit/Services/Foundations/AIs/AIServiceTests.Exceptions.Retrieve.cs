d3ad18f5e656f9fcb84ec6a4de2f56fb360df6bc
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2024-01-17 21:10:24 +0900

    ShouldThrowDependencyValidationExceptionOnRetrieveIfValidationErrorOccurs -> FAIL

---
+++ Standard.AI.Data.EntityIntelligence.Tests.Unit/Services/Foundations/AIs/AIServiceTests.Exceptions.Retrieve.cs
@@ -0,0 +1,54 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Standard.AI.OpenAI.Models.Clients.Completions.Exceptions;
+using Standard.AI.OpenAI.Models.Services.Foundations.Completions;
+using Standatd.AI.Data.EntityIntelligence.Models.Exceptions;
+using Xeptions;
+using Xunit;
+
+namespace Standard.AI.Data.EntityIntelligence.Tests.Unit.Services.Foundations.AIs
+{
+    public partial class AIServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnRetrieveIfValidationErrorOccurs()
+        {
+            // given
+            var someNaturalQuery = GetRandomString();
+            var someException = new Exception();
+
+            var completionClientValidationException =
+                new CompletionClientValidationException(someException as Xeption);
+
+            this.aiBrokerMock.Setup(broker =>
+                broker.PromptCompletionAsync(It.IsAny<Completion>()))
+                    .ThrowsAsync(completionClientValidationException);
+
+            var expectedAIQueryDependencyValidationException =
+                new AIQueryDependencyValidationException(completionClientValidationException);
+
+            // when
+            ValueTask<string> retrieveSqlQueryTask =
+                this.aiService.RetrieveSqlQueryAsync(someNaturalQuery);
+
+            AIQueryDependencyValidationException actualAIQueryDependencyValidationException =
+                await Assert.ThrowsAsync<AIQueryDependencyValidationException>(retrieveSqlQueryTask.AsTask);
+
+            // then
+            actualAIQueryDependencyValidationException.Should()
+                .BeEquivalentTo(expectedAIQueryDependencyValidationException);
+
+            this.aiBrokerMock.Verify(broker =>
+                broker.PromptCompletionAsync(
+                    It.IsAny<Completion>()), Times.Once);
+
+            this.aiBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Standatd.AI.Data.EntityIntelligence/Models/Exceptions/AIQueryDependencyValidationException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace Standatd.AI.Data.EntityIntelligence.Models.Exceptions
+{
+    public class AIQueryDependencyValidationException : Xeption
+    {
+        public AIQueryDependencyValidationException(Xeption innerException)
+            : base(message: "AI dependency validation error occurred, fix the errors and try again.")
+        { }
+    }
+}
